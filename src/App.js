a32e174846bea1f11b3908cd5551cdb35a15df58
maxus <maxus.tuguldur@gmail.com>
2019-05-05 18:16:57 +0800

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
-
+import { Navbar } from "./components";
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import "./style.css";
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1,
+    fontSize: "16px",
+    textTransform: "uppercase",
+    fontFamily: "Roboto Mono"
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 12
+  }
+};
+
+function Navbar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar className="dev-nav">
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <span className={classes.grow}>Програм Хангамж</span>
+          <Button color="inherit">Button</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+Navbar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Navbar);
