c49480fd9e0771c70078671b55028d32f04d6923
maxus <maxus.tuguldur@gmail.com>
2019-05-09 21:46:23 +0800

    Updates

---
+++ precache-manifest.a8f1d0e05164c0b68e8a5508de2dd0e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29df88d50c77e86c483dafd844e13523",
+    "url": "/stda/index.html"
+  },
+  {
+    "revision": "7ce73644c97203ab983d",
+    "url": "/stda/static/css/main.3053ee1b.chunk.css"
+  },
+  {
+    "revision": "c1c561c46008406d701d",
+    "url": "/stda/static/js/2.b4a5e99b.chunk.js"
+  },
+  {
+    "revision": "7ce73644c97203ab983d",
+    "url": "/stda/static/js/main.5865fffe.chunk.js"
+  },
+  {
+    "revision": "3c68e1b9f38859930335",
+    "url": "/stda/static/js/runtime~main.09041973.js"
+  }
+]);
