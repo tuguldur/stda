9c2e8cd6b70097f30a37bbe611e0e9c55f8f60c4
maxus <maxus.tuguldur@gmail.com>
2019-05-16 16:44:19 +0800

    Fixed Bugs\

--- src/containers/Team/index.js
+++ src/containers/Team/index.js
@@ -6,6 +6,31 @@ class Team extends Component {
     this.state = {};
   }
   render() {
+    const data = [
+      {
+        name: "name",
+        src:
+          "https://storage.googleapis.com/spec-host-backup/mio-material%2Fassets%2F1VKBitGP7fOOJO21ggWRhMp1QR81nhqff%2Fdarktheme-2x1.png",
+        alt: "tuguldur",
+        type: "Tuki",
+        link: "/gmail.com"
+      }
+    ];
+    const team = data.map((data, index) => (
+      <div className="tile" key={index}>
+        <a href={data.link} className="tile__link">
+          <figure className="tile__poster">
+            <div>
+              <img className="tile__image" src={data.src} alt={data.alt} />
+            </div>
+          </figure>
+          <h3 className="eyebrow">{data.type}</h3>
+          <h4>
+            <span className="tile__link">{data.name}</span>
+          </h4>
+        </a>
+      </div>
+    ));
     return (
       <section className="section">
         <div className="container container--jumbo">
@@ -19,102 +44,7 @@ class Team extends Component {
               </section>
             </div>
             <div className="multi-up multi-up--3 multi-up--slim teacher-tile">
-              <div className="tile">
-                <a href="#" className="tile__link">
-                  <figure class="tile__poster">
-                    <div>
-                      <img
-                        class="tile__image"
-                        src="https://storage.googleapis.com/spec-host-backup/mio-material%2Fassets%2F1VKBitGP7fOOJO21ggWRhMp1QR81nhqff%2Fdarktheme-2x1.png"
-                      />
-                    </div>
-                  </figure>
-                  <h3 className="eyebrow">Магистр</h3>
-                  <h4>
-                    <span class="tile__link">Овог Нэр</span>
-                  </h4>
-                </a>
-              </div>
-              <div className="tile">
-                <a href="#" className="tile__link">
-                  <figure class="tile__poster">
-                    <div>
-                      <img
-                        class="tile__image"
-                        src="https://storage.googleapis.com/spec-host-backup/mio-material%2Fassets%2F1VKBitGP7fOOJO21ggWRhMp1QR81nhqff%2Fdarktheme-2x1.png"
-                      />
-                    </div>
-                  </figure>
-                  <h3 className="eyebrow">Магистр</h3>
-                  <h4>
-                    <span class="tile__link">Овог Нэр</span>
-                  </h4>
-                </a>
-              </div>
-              <div className="tile">
-                <a href="#" className="tile__link">
-                  <figure class="tile__poster">
-                    <div>
-                      <img
-                        class="tile__image"
-                        src="https://storage.googleapis.com/spec-host-backup/mio-material%2Fassets%2F1VKBitGP7fOOJO21ggWRhMp1QR81nhqff%2Fdarktheme-2x1.png"
-                      />
-                    </div>
-                  </figure>
-                  <h3 className="eyebrow">Магистр</h3>
-                  <h4>
-                    <span class="tile__link">Овог Нэр</span>
-                  </h4>
-                </a>
-              </div>
-              <div className="tile">
-                <a href="#" className="tile__link">
-                  <figure class="tile__poster">
-                    <div>
-                      <img
-                        class="tile__image"
-                        src="https://storage.googleapis.com/spec-host-backup/mio-material%2Fassets%2F1VKBitGP7fOOJO21ggWRhMp1QR81nhqff%2Fdarktheme-2x1.png"
-                      />
-                    </div>
-                  </figure>
-                  <h3 className="eyebrow">Магистр</h3>
-                  <h4>
-                    <span class="tile__link">Овог Нэр</span>
-                  </h4>
-                </a>
-              </div>
-              <div className="tile">
-                <a href="#" className="tile__link">
-                  <figure class="tile__poster">
-                    <div>
-                      <img
-                        class="tile__image"
-                        src="https://storage.googleapis.com/spec-host-backup/mio-material%2Fassets%2F1VKBitGP7fOOJO21ggWRhMp1QR81nhqff%2Fdarktheme-2x1.png"
-                      />
-                    </div>
-                  </figure>
-                  <h3 className="eyebrow">Магистр</h3>
-                  <h4>
-                    <span class="tile__link">Овог Нэр</span>
-                  </h4>
-                </a>
-              </div>
-              <div className="tile">
-                <a href="#" className="tile__link">
-                  <figure class="tile__poster">
-                    <div>
-                      <img
-                        class="tile__image"
-                        src="https://storage.googleapis.com/spec-host-backup/mio-material%2Fassets%2F1VKBitGP7fOOJO21ggWRhMp1QR81nhqff%2Fdarktheme-2x1.png"
-                      />
-                    </div>
-                  </figure>
-                  <h3 className="eyebrow">Магистр</h3>
-                  <h4>
-                    <span class="tile__link">Овог Нэр</span>
-                  </h4>
-                </a>
-              </div>
+              {team}
             </div>
           </div>
         </div>
